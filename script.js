cf7ccd795bde84fa67dd2d2cf7c4c54cc29d696e
Mayadev3 <107764065+Mayadev3@users.noreply.github.com>
2022-10-31 19:14:53 +0100

    added ids to the inputs

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 //Submit event listener
-document
-  .getElementById(loan - form)
-  .addEventListener("submit", calculateResults);
+let form = document.getElementById("loan-form");
 
+form.addEventListener("submit", calculateResults);
 function calculateResults(e) {
   e.preventDefault();
+  const amount = document.getElementById("amount");
+  const interest = document.getElementById("interest");
+  const years = document.getElementById("years");
+  const monthlyPayment = document.getElementById("monthly-payment");
+  const totalPayment = document.getElementById("total-payment");
+  const totalInterest = document.getElementById("total-interest");
 }
